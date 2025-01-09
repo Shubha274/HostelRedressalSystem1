ebc6918cff13dc0a993b01936d03f35dc185f5ac
Chesta-2112343 <chesta121003@gmail.com>
2025-01-09 20:20:03 +0530

    Changed app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-
+import { useEffect } from "react";
 import StudentDashboard from "./Components/StudentDashboard/StudentDashboard";
 import Chart from "./Components/Chartss/Chart"
 function App() {
