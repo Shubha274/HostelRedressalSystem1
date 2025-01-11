f299cf52254b2c3847e465a6a15c378a9935bdb5
Shubha274 <pandeyshubha27@gmail.com>
2025-01-11 15:07:38 +0530

    added new

---
+++ client/src/Components/AdminDashboard/AdminDboard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Table from "../Tables/Table";
+import Navbar from "../NavBar/Navbar";
+import Sidebar from "../Sidebar/Sidebar";
+import ChatbotHeadWar from "../ChatbotHeadWar/ChatbotHeadWar";
+
+import Chart from "../Chartss/Chart";
+import { jwtDecode } from "jwt-decode";
+const token = localStorage.getItem("token");
+let role = null;
+
+// Decode the token to get the role if the token exists
+if (token) {
+  try {
+    const decodedToken = jwtDecode(token);
+    role = decodedToken.role; // Extract the role from the decoded token
+  } catch (error) {
+    console.error("Invalid token", error);
+  }
+}
+const AdminDboard = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>hello i am the admin</h1>
+      <ChatbotHeadWar />
+
+      <Chart />
+      <Table />
+      <Sidebar />
+    </div>
+  );
+};
+export default AdminDboard;
